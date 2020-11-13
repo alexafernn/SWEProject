03a54c98bd00c522398bac9392711d5155fc19eb
Jack Cannon <CannonJack17@gmail.com>
2020-11-13 18:28:46 -0500

    resized AddPet

--- app/src/main/java/com/alexandrafernandez/sweproject/AddPet.java
+++ app/src/main/java/com/alexandrafernandez/sweproject/AddPet.java
@@ -3,16 +3,27 @@ package com.alexandrafernandez.sweproject;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Switch;
+import android.widget.TextView;
 
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import org.w3c.dom.Text;
 
 public class AddPet extends AppCompatActivity {
 
     public static Pet p;
     public static Pets pets;
+    Spinner petTypeSpinner;
+    TextView pet_name, animal, pet_qualities, pet_sitting_logistics;
+    EditText pet_name_field, other_animal_type_field;
+    Button add_photo_button, settings_save_button;
+    Switch switch_energetic, switch_noisy, switch_trained, switch_inside_only;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +31,49 @@ public class AddPet extends AppCompatActivity {
         pets = new Pets();
         setContentView(R.layout.add_pet);
         setTitle("Add a New Pet");
+
+        ScreenSize view = new ScreenSize(this);
+
+        pet_name = (TextView) findViewById(R.id.pet_name);
+        pet_name.setTextSize(view.getLabelTextSize());
+
+        pet_name_field = (EditText) findViewById(R.id.pet_name_field);
+        pet_name_field.setTextSize(view.getEditTextSize());
+
+        animal = (TextView) findViewById(R.id.animal);
+        animal.setTextSize(view.getLabelTextSize());
+
+        petTypeSpinner = (Spinner) findViewById(R.id.pet_type_spinner);
+        ArrayAdapter<CharSequence> adapter4 = ArrayAdapter.createFromResource(this, R.array.pet_type_choices, android.R.layout.simple_spinner_item);
+        adapter4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        petTypeSpinner.setAdapter(adapter4);
+
+        other_animal_type_field = (EditText) findViewById(R.id.other_animal_type_field);
+        other_animal_type_field.setTextSize(view.getEditTextSize());
+
+        add_photo_button = (Button) findViewById(R.id.add_photo_button);
+        add_photo_button.setTextSize(view.getButtonTextSize());
+
+        pet_qualities = (TextView) findViewById(R.id.pet_qualities);
+        pet_qualities.setTextSize(view.getLabelTextSize());
+
+        switch_energetic = (Switch) findViewById(R.id.switch_energetic);
+        switch_energetic.setTextSize(view.getSwitchTextSize());
+
+        switch_noisy = (Switch) findViewById(R.id.switch_noisy);
+        switch_noisy.setTextSize(view.getSwitchTextSize());
+
+        switch_trained = (Switch) findViewById(R.id.switch_trained);
+        switch_trained.setTextSize(view.getSwitchTextSize());
+
+        pet_sitting_logistics = (TextView) findViewById(R.id.pet_sitting_logistics);
+        pet_sitting_logistics.setTextSize(view.getLabelTextSize());
+
+        switch_inside_only = (Switch) findViewById(R.id.switch_inside_only);
+        switch_inside_only.setTextSize(view.getSwitchTextSize());
+
+        settings_save_button = (Button) findViewById(R.id.settings_save_button);
+        settings_save_button.setTextSize(view.getButtonTextSize());
     }
 
     public void goToPets(View view)
@@ -30,23 +84,18 @@ public class AddPet extends AppCompatActivity {
         Boolean insideOnlyBoolean;
 
         //do something with entered info, then go back to pets menu
-        EditText animalName = (EditText) findViewById(R.id.pet_name_field);
-        EditText animalType = (EditText) findViewById(R.id.animal_field);
-        EditText otherInfo = (EditText) findViewById(R.id.other_quality_field);
-        Switch energetic = (Switch) findViewById(R.id.switch3);
-        Switch noisy = (Switch) findViewById(R.id.switch4);
-        Switch trained = (Switch) findViewById(R.id.switch5);
-        Switch insideOnly = (Switch) findViewById(R.id.switch6);
+        //EditText animalName = (EditText) findViewById(R.id.pet_name_field);
+        //Switch energetic = (Switch) findViewById(R.id.switch3);
+        //Switch noisy = (Switch) findViewById(R.id.switch4);
+        //Switch trained = (Switch) findViewById(R.id.switch5);
 
+        /*
         String strAnimalName = animalName.getText().toString();
-        String strAnimalType = animalType.getText().toString();
-        String strOtherInfo = otherInfo.getText().toString();
 
         if(energetic.isChecked())
             energeticBoolean= true;
         else
             energeticBoolean= false;
-
 
         if(noisy.isChecked())
             noisyBoolean= true;
@@ -58,12 +107,9 @@ public class AddPet extends AppCompatActivity {
         else
             trainedBoolean= false;
 
-        if(insideOnly.isChecked())
-            insideOnlyBoolean= true;
-        else
-            insideOnlyBoolean= false;
 
-        p = new Pet(strAnimalName, strAnimalType, energeticBoolean, noisyBoolean, trainedBoolean, insideOnlyBoolean, strOtherInfo);
+         */
+        //p = new Pet(strAnimalName, strAnimalType, energeticBoolean, noisyBoolean, trainedBoolean, insideOnlyBoolean, strOtherInfo);
         System.out.println("the pet u just made is " + p.toString());
         pets.addPettoList(p);
 
