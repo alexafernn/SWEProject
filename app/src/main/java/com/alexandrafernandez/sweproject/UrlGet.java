aed2dce97dd9b0aecd241ec72f349c2b5d23e3e6
Jack Cannon <CannonJack17@gmail.com>
2020-11-19 22:02:53 -0500

    attempted to fix server pull

--- app/src/main/java/com/alexandrafernandez/sweproject/UrlGet.java
+++ app/src/main/java/com/alexandrafernandez/sweproject/UrlGet.java
@@ -3,6 +3,7 @@ package com.alexandrafernandez.sweproject;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.util.Log;
 
 import java.io.InputStream;
 import java.net.URL;
@@ -18,18 +19,22 @@ public class UrlGet extends Thread {
         this.my_url = url;
         this.dataLocation = dataLocation;
         this.context = context;
+        Log.w("MA", "launched get:");
     }
 
     public void run( ) {
 
         try {
             URL url = new URL(my_url);
-            InputStream is = url.openStream();
+            InputStream is = url.openStream(); //failing on this line
             Scanner scan = new Scanner( is );
             String s = "";
             while(scan.hasNext()) {
                 s += scan.nextLine();
             }
+
+            Log.w("MA", "received from server:");
+            Log.w("MA", s);
 
             SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(context);
             SharedPreferences.Editor editor = pref.edit();
