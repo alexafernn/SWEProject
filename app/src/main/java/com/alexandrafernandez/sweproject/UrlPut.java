51184a998c346f712b141a8c276ee2f68a0089e0
Jack Cannon <CannonJack17@gmail.com>
2020-12-02 21:20:58 -0500

    get and post work

---
+++ app/src/main/java/com/alexandrafernandez/sweproject/UrlPut.java
@@ -0,0 +1,83 @@
+package com.alexandrafernandez.sweproject;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * UrlPut Class
+ * This class allows the app to modify data on the server
+ * CS482 Software Engineering
+ * Prof. Mehri
+ * @author Jack Cannon
+ * @author Alexandra Fernandez
+ * @version 3.0 Final Release
+ */
+public class UrlPut extends Thread{
+
+    /**
+     * Strings for managing changing url and data
+     */
+    String my_url, data, responseLocation;
+
+    /**
+     * Activity and View data
+     */
+    Context context;
+
+    /**
+     * UrlPost constructor
+     * @param url the url to send data to
+     * @param data the location to pull the request from
+     * @param context the relevant activity / view calling sending data
+     */
+    public UrlPut(String url, String data, Context context, String responseLocation) {
+        this.my_url = url;
+        this.data = data;
+        this.context = context;
+        this.responseLocation = responseLocation;
+    }
+
+    /**
+     * Run method
+     * Performs the url connection and post request
+     */
+    public void run( ) {
+
+        try {
+            URL url = new URL(my_url);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("PUT");
+            conn.setRequestProperty("Accept", "application/json");
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+            conn.setRequestProperty("Content-Type", "application/json; utf-8");
+            String params = data;
+            OutputStream os = conn.getOutputStream();
+            byte[] input = params.getBytes("utf-8");
+            os.write(input, 0, input.length);
+            BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line;
+            StringBuilder s= new StringBuilder();
+            while ((line = rd.readLine()) != null) {
+                Log.w("MA", "--------LINE:" + line);
+                s.append(line);
+            }
+            Log.w("MA", "Put request complete");
+            Log.w("MA", s.toString());
+
+            SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(context);
+            SharedPreferences.Editor editor = pref.edit();
+            editor.putString(responseLocation, s.toString());
+            editor.apply();
+
+        } catch( Exception e ) { Log.w("MA" , e.getMessage());}
+    }
+}
