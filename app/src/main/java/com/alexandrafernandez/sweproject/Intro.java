df8a79423b3dedaa062b8448e735a8a7de14b6c4
alexa <amfernandez@loyola.edu>
2020-12-03 21:19:49 -0500

    add logo and changed intro page to say HI + first name +!

--- app/src/main/java/com/alexandrafernandez/sweproject/Intro.java
+++ app/src/main/java/com/alexandrafernandez/sweproject/Intro.java
@@ -8,6 +8,7 @@ import android.content.res.Resources;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -18,6 +19,9 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Intro Class
@@ -40,6 +44,12 @@ public class Intro extends AppCompatActivity {
     Button owner, sitter, adoption;
 
     /**
+     * Server interaction objects
+     */
+    SharedPreferences pref;
+    String clientID, clientAuth;
+
+    /**
      * On Create Method
      * Initializes the intro View and instantiates other view objects for later use
      * @param savedInstanceState android system parameter
@@ -50,22 +60,56 @@ public class Intro extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setTitle("HOME");
 
+        //adding
+        //GET Request - get id/auth
+        pref = PreferenceManager.getDefaultSharedPreferences(this);
+        clientID = pref.getString("id", "");
+        clientAuth = pref.getString("auth", "");
+
+        //Url connection
+        UrlGet userInfo = new UrlGet("http://aiji.cs.loyola.edu/accountinfo?id=" + clientID + "&auth=" + clientAuth ,"profile.userInfo", this);
+        Log.w("MA", "--------Profile URL GET------------");
+        userInfo.start();
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        //Save response
+        String json = pref.getString("profile.userInfo", "");
+        Log.w("MA", "json: " + json);
+        boolean is_owner=false, is_sitter=false;
+        String first_name="";
+        try {
+            JSONObject jsonObject1 = new JSONObject(json);
+            first_name = jsonObject1.getString( "first_name" );
+        } catch( JSONException json_e ) {
+            Log.w("MA", json_e.toString());
+        }
+
+
+
+
+
+
         ScreenSize view = new ScreenSize(this);
 
-        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
-        String myName = pref.getString("username", "username");
+        //SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+        //String myName = pref.getString("username", "username");
 
         // need to get myName from the server
 
 
-        String greetMe = "Hi, " + myName;
+        //String greetMe = "Hi, " + myName;
+        String greetMe = "HI " + first_name.toUpperCase() + "!";
         greeting = (TextView) findViewById(R.id.greeting);
         greeting.setText(greetMe);
         greeting.setTextSize(view.getLabelTextSize());
 
-        status_messages = (TextView) findViewById(R.id.status_messages);
-        status_messages.setTextSize(view.getLabelTextSize());
-        status_messages.setHeight(view.getHeight()/3);
+//        status_messages = (TextView) findViewById(R.id.status_messages);
+//        status_messages.setTextSize(view.getLabelTextSize());
+//        status_messages.setHeight(view.getHeight()/3);
 
         owner = findViewById(R.id.owner_main_button); //i need a sitter
         owner.setTextSize(view.getButtonTextSize());
