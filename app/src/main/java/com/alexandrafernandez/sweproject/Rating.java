3926b0af3906843e6f40bb2c7e779b4fd7125e16
Jack Cannon <CannonJack17@gmail.com>
2020-12-09 18:49:39 -0500

    added rating activity

---
+++ app/src/main/java/com/alexandrafernandez/sweproject/Rating.java
@@ -0,0 +1,37 @@
+package com.alexandrafernandez.sweproject;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Rating extends AppCompatActivity {
+
+    Button b;
+
+    /**
+     * On Create Method
+     * Initializes the rating View and instantiates other view objects for later use
+     *
+     * @param savedInstanceState android system parameter
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.rating);
+        setTitle("Rate a user");
+
+        b = (Button) findViewById(R.id.rating_button);
+
+    }
+
+    /**
+     * On Submit Rating method
+     * Return to the previous activity after saving data
+     * @param view the reference object calling this method
+     */
+    public void onSubmitRating(View view) {
+        finish();
+    }
+}
